612b2b3eb5639d12c71e7ed00a6807924d884769
GabyNor05 <gabynorris616@gmail.com>
2026-06-22 03:02:59 +0200

    feat(backend): create routes and models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,27 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 
-dotenv.config()
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+import { connectDB } from "./lib/db.js";
 
-const app = express()
-const PORT = process.env.PORT
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server is running on port "+ PORT);
-    
-})
+  console.log("Server is running on port " + PORT);
+  connectDB();
+});
