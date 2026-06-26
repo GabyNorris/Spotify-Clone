5279061d2565b1b716c18fe982116cdbe333800f
GabyNor05 <gabynorris616@gmail.com>
2026-06-26 16:46:49 +0200

    feat(backend): added song, album, stat & user (Routes + Controllers)

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,10 @@
 import {Router} from "express"
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers, getMessages } from "../controllers/user.controller.js";
 
 const router = Router();
 
-
+router.get("/", protectRoute, getAllUsers);
+router.get("/messages", protectRoute, getMessages);
 
 export default router
